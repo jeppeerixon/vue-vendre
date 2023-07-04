f380a9703c797a5f36ea935eeed13a69b4be8ac4
Jesper Eriksson <84853548+jeppeerixon@users.noreply.github.com>
2023-07-04 08:45:49 +0200

    :tada: fetching data with typemodels

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import axios from 'axios';
+import { ref } from 'vue'
+import { IEmployees } from "./models/IEmployees";
+
+  const employees = ref<IEmployees[]>([])
+  const BASE_URL: string = 'https://reqres.in/api/users'
+
+  const fetchData = async (): Promise<IEmployees[]> => {
+    let response = await axios.get(`${BASE_URL}`);
+    return response.data
+  };
+
+  employees.value = await fetchData()
+
+
+  console.log(employees.value)
+
+</script>
+
+<template>
+  <p>hej</p>
+</template>
+
+<style scoped>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.vue:hover {
+    filter: drop-shadow(0 0 2em #42b883aa);
+  }
+</style>
---
+++ src/models/IEmployees.ts
@@ -0,0 +1,7 @@
+export interface IEmployees {
+    id: number;
+    email: string;
+    first_name: string;
+    last_name: string;
+    avatar: string;
+  }
